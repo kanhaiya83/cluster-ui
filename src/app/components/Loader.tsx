d9aea1ba11541a47464bde44ee063ba793c1e32f
Ubuntu <ubuntu@ip-172-31-37-248.ap-south-1.compute.internal>
2024-07-27 21:39:06 +0000

    loader fix

--- src/app/components/Loader.tsx
+++ src/app/components/Loader.tsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { RotatingSquare } from 'react-loader-spinner'
 
 const Loader = () => {
   return (
-    <div>Loader</div>
+    <div className='p-4 flex w-full justify-center mt-6'>
+      <RotatingSquare
+  visible={true}
+  height="100"
+  width="100"
+  color="#000"
+  ariaLabel="rotating-square-loading"
+  wrapperStyle={{}}
+  wrapperClass=""
+  />
+    </div>
   )
 }
 
