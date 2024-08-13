03ebd37a3ea3206f5db3fe5a8d48084f44e900ae
Kanhaiya <kanhaiya2778@gmail.com>
2024-08-13 16:43:34 +0530

    remove header

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <div className="flex flex-col justify-center self-stretch p-6 text-base shadow-sm backdrop-blur-sm bg-white bg-opacity-90 max-md:px-5">
       <div className="flex gap-5 justify-between py-0.5 pl-1.5 w-full max-md:flex-wrap max-md:max-w-full">
-      <Link
+      {/* <Link
         href={"/"}
         className="bg-gray-500 p-2 rounded-lg"
       >
@@ -14,7 +14,7 @@ const Header = () => {
           src="https://www.clusterprotocol.ai/assets/clusterProtocol.webp"
           className="shrink-0 my-auto w-32 "
         />
-      </Link>
+      </Link> */}
 
         <div className="flex flex-wrap gap-5 pl-3">
           <div className="flex flex-auto gap-5 justify-between my-auto whitespace-nowrap leading-[153.6%] text-zinc-500 max-md:flex-wrap">
